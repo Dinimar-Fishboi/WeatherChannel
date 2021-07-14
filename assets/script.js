3dd0777f786706dcbef4962896f375562ca4746a
Dinimar Fish <tayla@tallisman.com.au>
2021-07-14 18:51:02 +1000

    Added pseudocode, added qQueri js ?

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,20 @@
-console.log("Hello")
+//Need Latitude and Longitude to complete API call. Have added own API already
+
+var getAPI = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&15ded98cd7617010730249cbf7259a51"
+
+// We need to call a Latitude and Longitude when entering the names of locations.
+
+// We are going to need to add prev. searches to the local storage, that the 
+// user can reuse in the app (aka the info is Cached)
+
+// From the OpenWeatherMap API, we need to pull
+// 1. Name of place
+// 2. Date of Current day
+// 3. Icon reflective of weather (Font Awesome)
+// 4. Temperature
+// 5. Humidity
+// 6. Wind speed
+// 7. UV index (also add colour to reflect UV warnings)
+
+// We also need this for the next 5 days.
+
